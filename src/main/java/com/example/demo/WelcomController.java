6bdc5cba5cfe0aea69c18345e25c8cedc37f63aa
sriram <sriram.vuppuluri@gmail.com>
2021-08-24 19:33:57 +0530

    allow cors annotation

--- src/main/java/com/example/demo/WelcomController.java
+++ src/main/java/com/example/demo/WelcomController.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class WelcomController {
 
